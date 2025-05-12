19ade90e6fa25deb28ab46b67d813f5171008a28
dragotia_avl <adnand.dragoti@avl.com>
2025-05-12 10:46:59 +0200

    Implemented play sound. Game stopping should be reviewed

---
+++ Components/Pages/PacmanGame.razor.js
@@ -0,0 +1,9 @@
+﻿export function PlaySound() {
+    document.getElementById('game-sound').play();
+}
+
+export function StopSound() {
+    var audio = document.getElementById('game-sound');
+    audio.pause();
+    audio.currentTime = 0; // Reset the audio to the beginning
+}
